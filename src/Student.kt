5db20c47ae77c92d73b23ee6a670ab35a478a49d
kbea-sol <0beack69@solent.ac.uk>
2025-10-01 14:53:01 +0100

    added functionality to get student grade

--- src/Student.kt
+++ src/Student.kt
@@ -17,6 +17,17 @@ class Student (val id : Int, var name : String,
         return "Name: $name\nCourse: $course\nMark: $markIn"
     }
 
+    fun getGrade() : String{
+        return when(this.mark){
+            in 70..100 -> "First"
+            in 60..69 -> "2/1"
+            in 50..59 -> "2/2"
+            in 40..49 -> "Third"
+            in 0..39 -> "Fail"
+            else -> "Invalid mark"
+        }
+    }
+
 
 
 
--- src/main.kt
+++ src/main.kt
@@ -8,13 +8,20 @@ fun main()
     var students = arrayOf<Student>()
     var id : Int = 0
 
-    while(true)
-    {
-        id += 1
-        val name = ih.getString("Enter a name")
-        val course = ih.getString("Enter a course")
-        val mark = ih.getInt("Enter a mark")
-        students += Student(id, name, course, mark)
-        println(students[id-1])
-    }
+    val name = ih.getString("Enter student name")
+    val course = ih.getString("Enter student course")
+    val mark = ih.getInt("Enter student mark")
+    var stud = Student(1, name, course, mark)
+    println(stud.getGrade())
+
+
+//    while(true)
+//    {
+//        id += 1
+//        val name = ih.getString("Enter a name")
+//        val course = ih.getString("Enter a course")
+//        val mark = ih.getInt("Enter a mark")
+//        students += Student(id, name, course, mark)
+//        println(students[id-1])
+//    }
 }
