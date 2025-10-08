dbcc5ed286feb98d1d4d8a6ca01a5defbc305e95
kbea-sol <0beack69@solent.ac.uk>
2025-10-08 14:33:10 +0100

    created mutable list

--- src/main.kt
+++ src/main.kt
@@ -3,36 +3,37 @@ import InputHandlerCLI
 fun main()
 {
 
-
+    val liststud = mutableListOf<Student>()
     val ih : InputHandler = InputHandlerCLI()
 
-    val mark : Int= ih.getInt("Enter mark")
-
-    val stud = Master(1, "Test", "Test")
-    stud.mark = mark
-    println("Masters")
-    println(stud.getGrade())
-    println("Did pass: ${stud.didPass()}")
-
-    val mark2 : Int= ih.getInt("Enter mark")
-
-    val stud2 = Undergraduate(1, "Test", "Test")
-    stud2.mark = mark2
-    println("Undergrad")
-    println(stud2.getGrade())
-    println("Did pass: ${stud2.didPass()}")
-
-
+//    val mark : Int= ih.getInt("Enter mark")
+//
+//    val stud = Master(1, "Test", "Test")
+//    stud.mark = mark
+//    println("Masters")
 //    println(stud.getGrade())
+//    println("Did pass: ${stud.didPass()}")
+//
+//    val mark2 : Int= ih.getInt("Enter mark")
+//
+//    val stud2 = Undergraduate(1, "Test", "Test")
+//    stud2.mark = mark2
+//    println("Undergrad")
+//    println(stud2.getGrade())
+//    println("Did pass: ${stud2.didPass()}")
 
 
-//    while(true)
-//    {
-//        id += 1
-//        val name = ih.getString("Enter a name")
-//        val course = ih.getString("Enter a course")
-//        val mark = ih.getInt("Enter a mark")
-//        students += Student(id, name, course, mark)
-//        println(students[id-1])
-//    }
+    var id = 1
+    val noStudents = 3
+    for(i in 0..<noStudents)
+    {
+        id += 1
+        val name = ih.getString("Enter a name")
+        val course = ih.getString("Enter a course")
+        liststud.add(Undergraduate(id, name, course))
+    }
+    for(i in 0..<noStudents)
+    {
+        println(liststud[i])
+    }
 }
